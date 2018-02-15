562475e805b273b692a7748daaffd66f488a67a4
tatsumi kentaro <tatsuken4290@gmail.com>
2018-02-15 20:05:13 +0900

    アラート機能も実装

--- Swift4AnyMemo/RegisterViewController.swift
+++ Swift4AnyMemo/RegisterViewController.swift
@@ -19,9 +19,11 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //更新編集の場合
         if saveData.object(forKey: "contents") != nil{
             mainArray = saveData.object(forKey: "contents") as! [[String]]
         }
+        //新規保存か更新編集かを判定
         if cellOfNum != nil{
             titleTextField.text = mainArray[cellOfNum][0]
             contentsTextField.text = mainArray[cellOfNum][1]
@@ -30,6 +32,7 @@ class RegisterViewController: UIViewController {
             titleTextField.text = ""
             contentsTextField.text = ""
         }
+        //textViewに枠線をつける
         contentsTextField.layer.borderColor = UIColor.black.cgColor
         contentsTextField.layer.borderWidth = 1.0
         
@@ -40,13 +43,14 @@ class RegisterViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    //戻るボタン
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+    //保存ボタン
     @IBAction func saveButton(_ sender: Any) {
         contentsArray = [titleTextField.text!,contentsTextField.text!]
+        //新規保存か更新編集かを判定
         if cellOfNum != nil{
             mainArray.insert(contentsArray, at: cellOfNum)
             cellOfNum = nil
@@ -56,10 +60,19 @@ class RegisterViewController: UIViewController {
             print("B")
         }
         saveData.set(mainArray, forKey: "contents")
-        //        print(saveData.object(forKey: "contents") as! [[String]]!)
+        //保存を押した時のアラート
+        let alert:UIAlertController = UIAlertController(title: "保存", message: "保存しました", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            print("保存したよ")
+        }))
+        present(alert, animated: true, completion: nil)
         
         
     }
+    
+    
+    
+    
     
     
     
